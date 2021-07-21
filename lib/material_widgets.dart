6f2baf76a1049e88356275bb02077012d879dadd
LazyLazyMeat <LazyLazyMeat@yandex.ru>
2021-07-21 15:56:37 +0300

    Refactored endDrawer in "material_widgets.dart"

--- lib/material_widgets.dart
+++ lib/material_widgets.dart
@@ -32,101 +32,140 @@ class _MaterialWidgetScreenState extends State<MaterialWidgetScreen> {
     Icons.help_outline,
   ];
 
+  final Map<String, Color> colorMap = {
+    "drawerHeader" : Color(0xFF233040),
+    "drawerBackground" : Color(0xFF1C242F),
+    "entryTitle" : Color(0xFFECF0F1),
+    "entryLeading" : Color(0xFF727E8A),
+    "nickname" : Color(0xFFFEFEFE),
+    "phone" : Color(0xFF717D8B),
+    "themeSwitcher" : Color(0xFFFFFFFF),
+  };
+
+  final Map<String, Color> colorMapLight = {
+    "drawerHeader" : Color(0xFF5A8FBB),
+    "drawerBackground" : Color(0xFFFFFFFF),
+    "entryTitle" : Color(0xFF454545),
+    "entryLeading" : Color(0xFFAAB0B3),
+    "nickname" : Color(0xFFF4FCFD),
+    "phone" : Color(0xFFABD2EF),
+    "themeSwitcher" : Color(0xFFFAFEFD),
+  };
+
+  var _darkThemeUsing = true;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.of(context).pop(),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text('Material widgets demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.airline_seat_flat),
+            onPressed: () {
+              ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text('Ты пидор')));
+            },
           ),
-          title: const Text('Material widgets demo'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.airline_seat_flat),
-              onPressed: () {
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(const SnackBar(content: Text('Ты пидор')));
-              },
-            ),
-            DrawerButtonWidget(),
-          ]),
+          DrawerButtonWidget(),
+        ],
+      ),
       endDrawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              padding: EdgeInsets.all(0),
-              decoration: BoxDecoration(color: Color(0xFF233040)),
-              child: Container(
-                padding: EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Photo',
-                          style: TextStyle(color: Color(0xFFECF0F1)),
-                        ),
-                        Expanded(
-                          child: Text(''),
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.light_mode,
-                            color: Color(0xFFECF0F1),
+        child: Container(
+          color: _darkThemeUsing ? colorMap["drawerBackground"] : colorMapLight["drawerBackground"],
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                padding: EdgeInsets.all(0),
+                decoration: BoxDecoration(color: _darkThemeUsing ? colorMap["drawerHeader"] : colorMapLight["drawerHeader"]),
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            'Photo',
+                            style: TextStyle(color: _darkThemeUsing ? colorMap["nickname"] : colorMapLight["nickname"]),
+                          ),
+                          Expanded(
+                            child: Text(''),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.light_mode,
+                              color: _darkThemeUsing ? colorMap["themeSwitcher"] : colorMapLight["themeSwitcher"],
                             ),
-                          onPressed: () {},
-                          )
-                      ],
-                    ),
-                    Expanded(
-                      child: Text(''),
-                    ),
-                    Row(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                             'Alexey Bukin',
-                             style: TextStyle(color: Color(0xFFECF0F1)),
+                            onPressed: () {
+                              setState(() {
+                                _darkThemeUsing = _darkThemeUsing ? false : true;
+                              });
+                              },
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Text(''),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Alexey Bukin',
+                                style: TextStyle(
+                                  color: _darkThemeUsing ? colorMap["nickname"] : colorMapLight["nickname"],
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '+7 (920) 008-12-76',
+                                style: TextStyle(color: _darkThemeUsing ? colorMap["phone"] : colorMapLight["phone"]),
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Text(''),
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.keyboard_arrow_down_outlined,
+                              color: _darkThemeUsing ? colorMap["themeSwitcher"] : colorMapLight["themeSwitcher"],
                             ),
-                            Text(
-                              '+7 (920) 008-12-76',
-                              style: TextStyle(color: Color(0xFF727E8A)),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    
-                  ],
-                ),
-            ),  
-            ),
-            for (var i = 0; i < entryList.length; i++)
-              if (i != 6)
-                Container(
-                  decoration: BoxDecoration(color: Color(0xFF1C242F)),
-                  child: ListTile(
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),  
+              ),
+              for (var i = 0; i < entryList.length; i++)
+                if (i != 6)
+                  ListTile(
                     leading: Icon(
                       iconList[i],
-                      color: Color(0xFF727E8A),
-                      ),
+                      color: _darkThemeUsing ? colorMap["entryLeading"] : colorMapLight["entryLeading"],
+                    ),
                     title: Text(
                       entryList[i],
-                      style: TextStyle(color: Color(0xFFECF0F1)),
+                      style: TextStyle(
+                        color: _darkThemeUsing ? colorMap["entryTitle"] : colorMapLight["entryTitle"],
+                        fontWeight: FontWeight.bold
                       ),
+                    ),
                   )
-                )
-              else
-                Container(
-                  decoration: BoxDecoration(color: Color(0xFF1C242F)),
-                  child: Divider(),
-                )
-          ]
-        )
+                else
+                  Divider(),
+            ]
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(
@@ -140,14 +179,14 @@ class _MaterialWidgetScreenState extends State<MaterialWidgetScreen> {
         tooltip: 'Саня',
       ),
       body: Center(
-        child: Text('There must be prekol'),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+          child: const Text('Нажми меня'),
+          onPressed: () {},
+        ),
       ),
     );
   }
-}
-
-BoxDecoration drawerListTileDecoration() {
-  return BoxDecoration(color: Color(0xFF1C242F));
 }
 
 class DrawerButtonWidget extends StatelessWidget {
